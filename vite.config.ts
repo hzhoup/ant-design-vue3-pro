558786a5f34525733bc92a8a3c902a536a51ff33
hzp <hzhouping@outlook.com>
2023-02-08 16:12:24 +0800

    build: 项目别名配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path'
+
+const root = process.cwd()
+
+const resolvePath = (...paths: string[]) => resolve(__dirname, ...paths)
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  root,
   plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': resolvePath('src'),
+      '#': resolvePath('build')
+    }
+  }
 })
