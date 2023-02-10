57781cad60550ef25aefc0fd3104c9456a78e015
hzp <hzhouping@outlook.com>
2023-02-10 20:42:10 +0800

    feat: 布局主要内容配置

--- src/layouts/common/ContentLayout/index.vue
+++ src/layouts/common/ContentLayout/index.vue
@@ -1,6 +1,12 @@
 <template>
   <n-layout-content content-style="padding: 24px;">
-    <div>content</div>
+    <router-view v-slot="{ Component, route }">
+      <transition mode="out-in" :appear="true" name="fade-slide">
+        <keep-alive :include="[]">
+          <component :is="Component" :key="route.fullPath" />
+        </keep-alive>
+      </transition>
+    </router-view>
   </n-layout-content>
 </template>
 
--- src/styles/index.ts
+++ src/styles/index.ts
@@ -1,6 +1,7 @@
 import '@unocss/reset/tailwind.css'
 import 'uno.css'
 import './reset.scss'
+import './transition.scss'
 
 // eslint-disable-next-line @typescript-eslint/no-empty-function
 export function setupAssets() {}
